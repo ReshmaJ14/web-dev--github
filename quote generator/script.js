3e831a98968e96a27b19a13afbffc353c2c4dc5f
Reshma Janvalkar <reshmajanvalkar@gmail.com>
2024-02-11 13:46:05 +0530

    index.html

---
+++ quote generator/script.js
@@ -0,0 +1,16 @@
+let userName= "Reshma";
+let age=30;
+let hobbies= ['singing', 'dancing', 'reading'];
+let job={
+    fresher:'junior',
+    salary:'30000',
+    place: 'Mumbai'
+}
+alert(job.place)
+alert(hobbies[2]);
+let max;
+function calculateAge(){
+    max=age+20;
+}
+calculateAge();
+alert(max);
